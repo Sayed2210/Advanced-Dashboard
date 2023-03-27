bac3932dbeb549607d0e4eaedff78dac3d719642
Elsayed Hassan <eh600039@gmail.com>
2023-03-27 22:11:26 +0200

    create mega menu

--- stage/js/main.js
+++ stage/js/main.js
@@ -1,3 +1,4 @@
+
 //show hide side bar
 document.querySelector("i.fa-solid.fa-right-left").addEventListener("click",()=>{
     document.querySelector(".sidebar").classList.toggle("no-sidebar");
@@ -11,3 +12,40 @@ togglebtn.forEach((e)=>{
         document.querySelector(el.currentTarget.dataset.cont).classList.toggle("open")
     })
 })
+
+//fullscreen toggle
+document.querySelector(".fullscreen-toggle").addEventListener("click",(e)=>{
+    e.currentTarget.classList.toggle("fullscreen");
+    if(e.currentTarget.classList.contains("fullscreen")) {
+        openFullscreen();
+    }else {
+        // closeFullscreen();
+        document.exitFullscreen();
+        console.log("right")
+    }
+})
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+// /* Close fullscreen */
+// function closeFullscreen() {
+//   if (elem.exitFullscreen) {
+//     elem.exitFullscreen();
+//   } else if (elem.webkitExitFullscreen) { /* Safari */
+//     elem.webkitExitFullscreen();
+//   } else if (elem.msExitFullscreen) { /* IE11 */
+//     elem.msExitFullscreen();
+//   }
+// }
